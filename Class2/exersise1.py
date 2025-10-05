2d063c452900e90e7a39ffee7ed0a5174861e1fa
joshua-waa <Joshuacheng1@outlook.com>
2025-10-05 18:04:45 -0400

    Create exersise1.py
    Make a code showing what bob got!

---
+++ Class2/exersise1.py
@@ -0,0 +1,18 @@
+while True:
+    score=input("What is Tom's score?")
+    try:
+        score=int(score)
+        break
+    except ValueError:
+        print("Put in a integer pls.")
+
+if score==100:
+    print("Wow he got perfect!")
+elif score>=90:
+    print("Wow he did very well!")
+elif score>=80:
+    print("Wow he did well!")
+elif score>=60:
+    print("Wow he passed!")
+else:
+    print("He failed.")
