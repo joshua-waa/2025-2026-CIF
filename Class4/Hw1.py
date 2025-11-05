166c568e32ef525b9b9df92b4707028eda6d7644
joshua-waa <Joshuacheng1@outlook.com>
2025-11-04 21:24:00 -0500

    Create Hw1.py
    Make a TBAG(Text Base Adventure Game)

---
+++ Class4/Hw1.py
@@ -0,0 +1,33 @@
+
+import random
+q=["What is the initials of the first american president?(Eg: abraham lincon= al)","What year is it?","Name the most popular ice-cream flavor.","What is (9+6)/3?","Name the largest web browser.","What is the value of π (two decimals)?","What do bees collect from flowers?","What is the largest ocean on Earth?(one word)","What’s H₂O commonly called?"]
+a=["gw","2025","vanilla","5","chrome","3.14","nectar","pacific","water"]
+lives=int(3)
+al=[]
+def intro():
+    print("Welcome to a TBAG game.")
+    print("Here are the instructions")
+    print("1.Only use lower-case letters\n2.Only type things to answer the question, no unnecessary words or spaces.\n3.You only have 3 lives.\n4.Have fun!\nQuestion time!")
+for i in range(len(q)):
+    again=1
+    while again==1:
+        n=random.randint(0,len(q)-1)
+        if n in al:
+            again=1
+        else:
+            again=0
+        al.append(n)
+    answer=input(q[n])
+    if answer==a[n]:
+        print("You got it correct!")
+        print(f"You have {lives} lives left.")
+    else:
+        lives -=1
+        print("You got it wrong.")
+        print(f"The correct answer is {a[n]}.")
+        print(f"You have {lives} lives left.")
+
+    if lives==0:
+        print("You ran out of lives!")
+        break
+print("You finished the game!")
