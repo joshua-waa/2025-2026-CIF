75a085087e2907ea114111bf071265291b72dfa6
joshua-waa <Joshuacheng1@outlook.com>
2025-11-14 19:15:23 -0500

    Idk add comments to the code?

---
+++ Class6/Hw2.py
@@ -0,0 +1,9 @@
+tbag_questions = [# random questions in a list
+    ["True/False: Europe has no deserts. ", ["True", "true"]],
+    ["How many seasons does Friends have? ", ["10", "Ten", "ten"]],
+    ["What is the largest organ? ", ["skin", "Skin"]],
+    ["What is the most copied link on the internet? (just write one word) ", ["rickroll", "Rickroll", "RickRoll"]],
+    ["True/False: Albert Einstein had trouble with mathematics in school. ", ["False", "false"]],
+    ["What YouTube video has the most views? ", ["Baby Shark Dance", "baby shark dance"]],
+    ["How many hearts does an octopus have? ", ["3", "three", "Three"]]
+]
