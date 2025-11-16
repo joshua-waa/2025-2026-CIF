d024939267e5de321febadceec298858ae7464f0
joshua-waa <Joshuacheng1@outlook.com>
2025-11-16 17:32:23 -0500

    Make a square using turtle

---
+++ Class7/Exercise1.py
@@ -0,0 +1,18 @@
+import turtle
+turtle.speed(0)
+def forward(a):
+    turtle.forward(a)
+def left(a):
+    turtle.left(a)
+def right(a):
+    turtle.right(a)
+
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
+
+turtle.done()
