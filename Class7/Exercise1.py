0007a20ced6b2d5cf4e94e3a9574acf81e8bd932
joshua-waa <Joshuacheng1@outlook.com>
2025-11-23 17:30:21 -0500

    Idk

--- Class7/Exercise1.py
+++ Class7/Exercise1.py
@@ -1,1 +1,37 @@
+import turtle
 
+
+turtle.bgcolor("black")
+turtle.pencolor("white")
+turtle.speed(0)
+turtle.shape("circle")
+turtle.shapesize(1)
+def forward(a):
+    turtle.forward(a)
+def left(a):
+    turtle.left(a)
+def right(a):
+    turtle.right(a)
+turtle.penup()
+turtle.goto(150,-150)
+turtle.fillcolor("red")
+turtle.begin_fill()
+turtle.pendown()
+for i in range(4):
+    forward(100)
+    right(90)
+turtle.end_fill()
+forward(50)
+left(90)
+turtle.fillcolor("white")
+turtle.speed(5)
+for i in range(90):
+    left(1)
+    forward(3.7)
+turtle.shapesize(1)
+turtle.color("yellow")
+for i in range(10):
+    turtle.pensize(2*i)
+
+
+turtle.done()
