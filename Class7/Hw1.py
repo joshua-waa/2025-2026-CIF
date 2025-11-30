468bde199f1f75d9a40357fb99dbe55661bcb8a0
joshua-waa <Joshuacheng1@outlook.com>
2025-11-30 18:51:15 -0500

    HW

---
+++ Class7/Hw1.py
@@ -0,0 +1,8 @@
+#Mabe use rect or smth to make a rectangle and assign the rect to variables and add or substract the var to move the rect.
+#like:
+rect=make.rect(200, 200)
+rect.pos(a,b)
+while True:
+  pygame.display.update()
+  a-=1
+  # makes box go left
