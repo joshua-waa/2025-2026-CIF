b290409901fa5e653cbb38f8494a7be588e02fe3
joshua-waa <Joshuacheng1@outlook.com>
2025-12-14 17:34:52 -0500

    Update Pygame starter kit.py

--- Pygame starter kit.py
+++ Pygame starter kit.py
@@ -1,26 +1,19 @@
 import pygame
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((800, 600)) #size
 clock = pygame.time.Clock()
 playing = True
-x=0
-y=0
+x=?
+y=?
 while playing:
-    clock.tick(30)
+    clock.tick(30) #fps
     key = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             playing = False
-    if key[pygame.K_UP]:
-        y-=4
-    if key[pygame.K_DOWN]:
-        y+=4
-    if key[pygame.K_LEFT]:
-        x-=4
-    if key[pygame.K_RIGHT]:
-        x+=4
-    screen.fill((0, 0, 0))
-    grect=pygame.Rect(x,y,100,80)
-    pygame.draw.rect(screen,(0,255,0),grect)
+    #if key[pygame.K_UP]:    is for controllable things
+    screen.fill((0, 0, 0))  # Fills with black
+    rect = pygame.Rect(x, y, width, height)
+    pygame.draw.rect(screen, (color rgb), rect)
     pygame.display.update()
 pygame.quit()
