f6925e0977114996aad9cdc003e683250be2f3ab
joshua-waa <Joshuacheng1@outlook.com>
2026-01-17 22:37:53 -0500

    Dont judge the kashuhua img

--- Pygame starter kit.py
+++ Pygame starter kit.py
@@ -1,14 +1,16 @@
-############################THINGS WITH THIS HASHTAGS ARE OPTIONAL
-
+####### IMPORTANT
+###### COMMENTED OR # IS OPTIONAL
+# For moving boxes with controls is red
+# # is text
+# ################################### is pls change
+from ctypes.wintypes import PWORD
+from turtledemo.nim import SCREENWIDTH
 
 import pygame
-from pygame import K_DOWN, K_LEFT, K_RIGHT
 
 pygame.init()
 
-#text
-font = pygame.font.SysFont("Comic Sans MS", 30) ##################################
-textsurface = font.render("PUT YOUR TEXT HERE", False, (255, 255, 255))#########################
+
 
 # screen size
 screenwidth = 800
@@ -16,21 +18,36 @@ screenheight = 600
 screen = pygame.display.set_mode((screenwidth, screenheight))
 bgcolor=(0,0,0)
 
+#text/images
+img_w=768   #centre the img
+img_h=432   #centre the img
+img_x = screenwidth // 2 - img_w // 2   #centre the img
+img_y = screenheight // 2 - img_h // 2   #centre the img
+
+image = pygame.image.load("Kashuhua.png").convert_alpha()
+image = pygame.transform.scale(image, (img_w, img_h))
+
+
+
+font=pygame.font.SysFont("Comic Sans MS", 30)
+word_color=(255,255,255)
+text="PUT YOUR TEXT HERE" ###################################################
+
 clock = pygame.time.Clock()
 playing = True
 
 # box position
-x = 0  ###########
-y = 0  ######################
-width = 80   # box width##############
-height = 80  # box height#################
-speed = 4    # speed of the box#####
+x = 0
+y = 0
+width = 80   # box width ################################################
+height = 80  # box height ############################################
+speed = 4    # speed of the box ################################################
 
 # movement flags
-move_up = False  ###############
-move_down = False    ###########
-move_left = False    ###########
-move_right = False    ###########
+move_up = False
+move_down = False
+move_left = False
+move_right = False
 
 while playing:
 
@@ -41,66 +58,75 @@ while playing:
             playing = False
 
         # key pressed
-        if event.type == pygame.KEYDOWN:     ###########
-            if event.key == pygame.K_w or K_up:
+        if event.type == pygame.KEYDOWN:
+            if event.key in (pygame.K_w, pygame.K_UP):
                 move_up = True
-            elif event.key == pygame.K_s or K_DOWN:
+            elif event.key in (pygame.K_s, pygame.K_DOWN):
                 move_down = True
-            elif event.key == pygame.K_a or K_LEFT:
+            elif event.key in (pygame.K_a, pygame.K_LEFT):
                 move_left = True
-            elif event.key == pygame.K_d or K_RIGHT:
+            elif event.key in (pygame.K_d, pygame.K_RIGHT):
                 move_right = True
 
         # key released
-        if event.type == pygame.KEYUP:    ###########
-            if event.key == pygame.K_w or K_up:
+        if event.type == pygame.KEYUP:
+            if event.key in (pygame.K_w, pygame.K_UP):
                 move_up = False
-            elif event.key == pygame.K_s or K_DOWN:
+            elif event.key in (pygame.K_s, pygame.K_DOWN):
                 move_down = False
-            elif event.key == pygame.K_a or K_LEFT:
+            elif event.key in (pygame.K_a, pygame.K_LEFT):
                 move_left = False
-            elif event.key == pygame.K_d or K_RIGHT:
+            elif event.key in (pygame.K_d, pygame.K_RIGHT):
                 move_right = False
 
-    dx = 0      ###########
-    dy = 0    ###########
+    dx = 0 #direction(1 is right 0 is left)
+    dy = 0 #direction(1 is down 0 is up)
     # horizontal movement
-    if move_left:    ###########
+    if move_left:
         dx -= 1
-    if move_right:    ###########
+    if move_right:
         dx += 1
 
     # vertical movement
-    if move_up:    ###########
+    if move_up:
         dy -= 1
-    if move_down:    ###########
+    if move_down:
         dy += 1
 
-    if dx != 0 or dy != 0:  #to prevent dividing by 0    ###########    ###########    ###########    ###########    ###########
-        length = (dx **2 + dy**2) ** 0.5  # ** is power of   # pythagorean theorem    ###########    ###########
-        dx = dx / length   # make distance for movement *1.???? for speed    ###########    ###########
-        dy = dy / length   # make distance for movement *1.???? for speed    ###########    ###########
-        x += dx * speed    ###########    ###########
-        y += dy * speed    ###########    ###########
+    if dx != 0 or dy != 0:  #to prevent dividing by 0
+        length = (dx **2 + dy**2) ** 0.5  # ** is power of   # pythagorean theorem
+        dx = dx / length   # make distance for movement *1.???? for speed
+        dy = dy / length   # make distance for movement *1.???? for speed
+        x += dx * speed
+        y += dy * speed
 
     screen.fill(bgcolor)  # background color
 
     # text
-    screen.blit(textsurface, (0, 0))     ###########
+    screen.blit(font.render(text, True, word_color), (0, 0))################
+    screen.blit(image, (img_x, img_y))
 
-    #borders
-    if x < 0:     ###########    ###########    ###########
-        x = 0    ###########    ###########    ###########    ###########
-    if y < 0:    ###########    ###########    ###########
-        y = 0    ###########    ###########    ###########
-    if x > screenwidth - width:    ###########    ###########    ###########
-        x = screenwidth - width    ###########    ###########    ###########
-    if y > screenheight - height:    ###########    ###########    ###########
-        y = screenheight - height    ###########    ###########    ###########
 
-    # draw box
-    rect = pygame.Rect(x, y, width, height)    ###########    ###########    ###########
-    pygame.draw.rect(screen, (255, 255, 255), rect)    ###########    ###########    ###########
+    if x < 0:
+        x = 0
+    if y < 0:
+        y = 0
+    if x > screenwidth - width:
+        x = screenwidth - width
+    if y > screenheight - height:
+        y = screenheight - height
+
+    #before movement/ wasd detection
+    #old_x=x
+    #old_y=y
+
+    #for collision, use
+    #if rect.colliderect(rect2):
+        #x=old_x
+        #y=old_y
+
+    rect = pygame.Rect(x, y, width, height)    # draw box
+    pygame.draw.rect(screen, (255, 255, 255), rect)    # draw box
 
     pygame.display.update()
 
